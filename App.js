8c9fc945417d5c916879e1e14bf4efb9373b2b12
Gordon Sun <gsun@snackv.com>
2019-01-18 15:42:06 -0800

    Load using loadAsync with old MediaPlayer

--- App.js
+++ App.js
@@ -2,14 +2,27 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Video } from 'expo';
 
-
 export default class App extends React.Component {
+  playerRef = undefined
+  async componentDidMount() {
+    try {
+      await this.playerRef.loadAsync(
+        {uri: 'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4'},
+        {shouldPlay: true, androidImplementation: 'MediaPlayer'}
+      )
+      console.log('load async success')
+    }
+    catch(e){
+      console.log('load async failed')
+      console.log(e)
+    }
+  }
   render() {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
         <Video
-          source={{ uri: 'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4' }}
+          // source={{ uri: 'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4' }}
           rate={1.0}
           volume={1.0}
           isMuted={false}
@@ -17,6 +30,7 @@ export default class App extends React.Component {
           shouldPlay
           isLooping
           style={{ width: 300, height: 300 }}
+          ref={(ref) => {this.playerRef = ref}}
         />
       </View>
     );
